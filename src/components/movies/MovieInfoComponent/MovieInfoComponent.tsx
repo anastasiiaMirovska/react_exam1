c6b67ba6e1eb507df110659f1c873680e985e22b
Anastasiia Mirovska <anastasiia.mirovska@gmail.com>
2024-06-24 18:30:10 +0300

    Revert "MovieInfo"
    This reverts commit 3c47aee6bbe9760ad66f72a38deab2a61462829c.

--- src/components/movies/MovieInfoComponent/MovieInfoComponent.tsx
+++ src/components/movies/MovieInfoComponent/MovieInfoComponent.tsx
@@ -1,12 +1,13 @@
 import React, {FC} from 'react';
+import {useAppSelector} from "../../../hooks/reduxHooks";
+import {IMovie} from "../../../Interfaces/IMovie";
 import {IMovieDetails} from "../../../Interfaces/IMovieDetails";
 import PosterPreviewComponent from "../../elements/PosterPreviewComponent/PosterPreviewComponent";
+import {useNavigate} from "react-router-dom";
 import StarsRatingComponent from "../../elements/StarsRatingComponent/StarsRatingComponent";
 import ButtonBack from "../../elements/ButtonBack/ButtonBack";
 import {Badge} from "@mui/material";
 import GenreBadgeComponent from "../../genres/GenreBadgeComponent/GenreBadgeComponent";
-
-import styles from "./MovieInfoComponent.module.css"
 
 interface IProps{
     movie : IMovieDetails
@@ -20,27 +21,26 @@ const MovieInfoComponent:FC<IProps> = ({movie}) => {
 
             <ButtonBack/>
             <h2>{movie.title}</h2>
-            <div className={styles.MovieInfoBox}>
-                <div className={styles.GeneralInfo}>
-                    <span>Age : {movie.adult ? <span>for adults</span> : <span>any</span>}</span><br/>
-                    <span>ID: {movie.id}</span><br/>
-                    <span>Original language: {movie.original_language}</span><br/>
-                    <span>Original title: {movie.original_title}</span><br/>
-                    <span>Overview: {movie.overview}</span><br/>
-                    <span>Popularity: {movie.popularity}</span><br/>
-                    <span>Release date: {movie.release_date}</span><br/>
-                    <span>Vote count: {movie.vote_count}</span><br/>
-                    <StarsRatingComponent rating={rating} size={"large"}/>
-                    <span>Rating: {rating}</span>
-                </div>
-                <div className={styles.PosterBox}>
-                    <Badge color="success" anchorOrigin={{vertical: 'top', horizontal: 'right',}}>
-                        <GenreBadgeComponent genres={movie.genres}/>
-                        <PosterPreviewComponent poster_path={movie.poster_path} poster_size={2}/>
-                    </Badge><br/>
-                </div>
+
+            <div>
+                <Badge color="success" anchorOrigin={{vertical: 'top', horizontal: 'right',}}>
+                    <GenreBadgeComponent genres={movie.genres}/>
+                    <PosterPreviewComponent poster_path={movie.poster_path} poster_size={2}/>
+                </Badge><br/>
             </div>
 
+            <div>
+                <span>Age : {movie.adult ? <span>for adults</span> : <span>any</span>}</span><br/>
+                <span>ID: {movie.id}</span><br/>
+                <span>Original language: {movie.original_language}</span><br/>
+                <span>Original title: {movie.original_title}</span><br/>
+                <span>Overview: {movie.overview}</span><br/>
+                <span>Popularity: {movie.popularity}</span><br/>
+                <span>Release date: {movie.release_date}</span><br/>
+                <span>Vote count: {movie.vote_count}</span><br/>
+                <StarsRatingComponent rating={rating} size={"large"}/>
+                <span>Rating: {rating}</span>
+            </div>
 
         </div>
     );
